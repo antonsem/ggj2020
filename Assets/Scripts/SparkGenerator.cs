e658e0c8eb4296f355dcaca7626bf386fda0e3a6
Anton <anton.semchenko@ar-vizor.cz>
2020-02-01 15:40:59 +0100

    Sparks

---
+++ Assets/Scripts/SparkGenerator.cs
@@ -0,0 +1,30 @@
+﻿using ExtraTools;
+using UnityEngine;
+
+public class SparkGenerator : Singleton<SparkGenerator>
+{
+    [SerializeField, MyBox.ReadOnly]
+    private float timer = 0;
+    [SerializeField]
+    private float defaultTimer = 0.5f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (timer > 0) return;
+        timer = defaultTimer;
+
+        if (collision.contacts.Length > 0)
+        {
+            ParticleSystem sys = ExtraTools.ResourceManager.Instance.spark.Get(collision.contacts[0].point).GetComponent<ParticleSystem>();
+            sys.Play();
+            sys.transform.up = collision.contacts[0].normal;
+
+        }
+    }
+
+    private void Update()
+    {
+        if (timer > 0)
+            timer -= Time.deltaTime;
+    }
+}
